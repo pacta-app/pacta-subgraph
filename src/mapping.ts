915177b557c88697da9f511ac67a27a6e58a375c
Simon Buechi <simon.buechi@gmail.com>
2019-12-28 14:03:30 +0100

    Update mapping.ts

--- src/mapping.ts
+++ src/mapping.ts
@@ -1,1 +1,10 @@
 
+import { NewAssocation} from '../generated/Pacta/Pacta'
+import { Organisation} from '../generated/schema'
+
+export function handleNewAssocation(event: NewAssocation): void {
+  let organisation = new Organisation(event.params.id.toHex())
+  organisation.name = event.params.name
+  organisation.address = event.params.association
+  organisation.save()
+}
