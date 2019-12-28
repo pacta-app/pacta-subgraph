11f7a51dd995c847201795f9d808dc29414cbd05
Simon Buechi <simon.buechi@gmail.com>
2019-12-28 15:04:36 +0100

    updates

--- src/mapping.ts
+++ src/mapping.ts
@@ -1,10 +1,65 @@
+import { BigInt } from "@graphprotocol/graph-ts"
+import {
+  Contract,
+  newAssociation,
+  feePaid,
+  ownedPaymentReceived,
+  ownedFallback
+} from "../generated/Pacta/Pacta"
+import { Organisation } from "../generated/schema"
 
-import { NewAssocation} from '../generated/Pacta/Pacta'
-import { Organisation} from '../generated/schema'
+export function handlenewAssociation(event: newAssociation): void {
+  // Entities can be loaded from the store using a string ID; this ID
+  // needs to be unique across all entities of the same type
+  let entity = Organisation.load(event.transaction.from.toHex())
 
-export function handleNewAssocation(event: NewAssocation): void {
-  let organisation = new Organisation(event.params.id.toHex())
-  organisation.name = event.params.name
-  organisation.address = event.params.association
-  organisation.save()
+  // Entities only exist after they have been saved to the store;
+  // `null` checks allow to create entities on demand
+  if (entity == null) {
+    entity = new Organisation(event.transaction.from.toHex())
+
+    // Entity fields can be set using simple assignments
+    entity.count = BigInt.fromI32(0)
+  }
+
+  // BigInt and BigDecimal math are supported
+  entity.count = entity.count + BigInt.fromI32(1)
+
+  // Entity fields can be set based on event parameters
+  entity.name = event.params.name
+  entity.association = event.params.association
+
+  // Entities can be written to the store with `.save()`
+  entity.save()
+
+  // Note: If a handler doesn't require existing field values, it is faster
+  // _not_ to load the entity from the store. Instead, create it fresh with
+  // `new Entity(...)`, set the fields that should be updated and save the
+  // entity back to the store. Fields that were not set or unset remain
+  // unchanged, allowing for partial updates to be applied.
+
+  // It is also possible to access smart contracts from mappings. For
+  // example, the contract that has emitted the event can be connected to
+  // with:
+  //
+  // let contract = Contract.bind(event.address)
+  //
+  // The following functions can then be called on this contract to access
+  // state variables and other data:
+  //
+  // - contract.creationDate(...)
+  // - contract.version(...)
+  // - contract.associations(...)
+  // - contract.getAssociations(...)
+  // - contract.getAssociationOwner(...)
+  // - contract.manager(...)
+  // - contract.migrateAssociations(...)
+  // - contract.getBeneficiaries(...)
+  // - contract.getOwner(...)
 }
+
+export function handlefeePaid(event: feePaid): void {}
+
+export function handleownedPaymentReceived(event: ownedPaymentReceived): void {}
+
+export function handleownedFallback(event: ownedFallback): void {}
